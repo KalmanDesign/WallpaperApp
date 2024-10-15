53f4d9999610c9ecd2887d50f52c132deb5c70c9
KalmanDesign <kalman0715@gmail.com>
2024-10-15 23:37:05 +0800

    1.添加app Icon，2.修改启动页设计，3.自动创建相册并将下载的图片自动保存到该相册。

---
+++ Wallpaper/Config/Configuration.swift
@@ -0,0 +1,8 @@
+//
+//  Configuration.swift
+//  Wallpaper
+//
+//  Created by Kalman on 2024/10/15.
+//
+
+import Foundation
---
+++ Wallpaper/ViewModel/PhotoManager.swift
@@ -0,0 +1,8 @@
+//
+//  PhotoManager.swift
+//  Wallpaper
+//
+//  Created by Kalman on 2024/10/15.
+//
+
+import Foundation
